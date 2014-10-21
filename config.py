bdc740891ec615f002c6001d4707e00b03d78f79
David Tomaschik <david@systemoverlord.com>
2014-10-21 10:24:24 -0700

    Add dev-oriented config.py defaults.

---
+++ config.py
@@ -0,0 +1,5 @@
+# Demo config.py, please configure your own
+SQLALCHEMY_DATABASE_URI = 'sqlite:///scoreboard.db'
+SECRET_KEY = 'CHANGEME CHANGEME CHANGEME'
+TITLE = 'CTF Scoreboard Dev'
+TEAMS = True
