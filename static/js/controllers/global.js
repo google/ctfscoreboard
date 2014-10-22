3b5069747a680fd1bb7f8b5809d7e7f97d251aa1
David Tomaschik <david@systemoverlord.com>
2014-10-22 16:40:25 -0700

    Track if news items are new.

--- static/js/controllers/global.js
+++ static/js/controllers/global.js
@@ -48,8 +48,19 @@ globalCtrls.controller('NewsCtrl', [
     '$scope',
     'newsService',
     function($scope, newsService) {
+        $scope.latest = 0;
         var updateNews = function(newsItems) {
+            var latest = 0;
+            angular.forEach(newsItems, function(item) {
+                var d = Date.parse(item.timestamp);
+                if (d > latest)
+                  latest = d;
+            });
             $scope.newsItems = newsItems;
+            if (latest > $scope.latest) {
+                // TODO: call attention to new news
+                $scope.latest = latest;
+            }
         };
 
         newsService.registerClient(updateNews);
