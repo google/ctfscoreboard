8b30e77d048100e48e89ab85353fd3573f769e1d
David Tomaschik <david@systemoverlord.com>
2016-01-28 23:24:03 -0800

    Revert config.py to a more common configuration.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Demo config.py, please configure your own
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://scoreboard:pass123word@localhost/scoreboard'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/scoreboard.db'
 SECRET_KEY = 'CHANGEME CHANGEME CHANGEME'
 TITLE = 'CTF Scoreboard Dev'
 TEAMS = True
 ATTACHMENT_BACKEND = 'file:///tmp/attachments'
-LOGIN_METHOD = 'appengine'
+LOGIN_METHOD = 'local'
