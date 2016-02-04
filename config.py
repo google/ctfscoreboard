79847c5f1d02a16b574d01101199930726f98208
David Tomaschik <david@systemoverlord.com>
2016-02-04 09:52:51 -0800

    Suppress warnings.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Demo config.py, please configure your own
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/scoreboard.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'CHANGEME CHANGEME CHANGEME'
 TITLE = 'CTF Scoreboard Dev'
 TEAMS = True
