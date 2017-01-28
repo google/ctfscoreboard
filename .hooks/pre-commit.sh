28f901d5030197323ba53f60011d0850539f39b6
David Tomaschik <david@systemoverlord.com>
2017-01-28 14:49:30 -0800

    Fix stash in pre-commit hook.

--- .hooks/pre-commit.sh
+++ .hooks/pre-commit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # stash code not to be committed
-git stash -q -keep-index
+git stash -q --keep-index >/dev/null 2>&1
 
 # Run tests and flake8
 python main.py runtests && flake8 .
 RESULT=$?
 
 # restore stash
-git stash pop -q
+git stash pop -q >/dev/null 2>&1
 
 exit $RESULT
