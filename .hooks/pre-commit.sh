1f3fcaf35e255c6c522a12d17af1ca59a15fb5a4
David Tomaschik <david@systemoverlord.com>
2017-02-08 20:20:32 -0800

    Only run tests if python code changes.

--- .hooks/pre-commit.sh
+++ .hooks/pre-commit.sh
@@ -3,9 +3,13 @@
 # stash code not to be committed
 git stash -q --keep-index >/dev/null 2>&1
 
-# Run tests and flake8
-python main.py runtests && flake8 .
-RESULT=$?
+RESULT=0
+
+if git ls-files -m . | grep -q '^scoreboard/' ; then
+  # Run tests and flake8 if any files in scoreboard/... changed.
+  python main.py runtests && flake8 .
+  RESULT=$?
+fi
 
 # restore stash
 git stash pop -q >/dev/null 2>&1
