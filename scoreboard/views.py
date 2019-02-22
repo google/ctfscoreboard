6fe4c57ecdc098372d917527a1d462267039388d
David Tomaschik <david@systemoverlord.com>
2019-02-22 00:04:23 -0600

    Allow anonymous users to download attachments.
    This still validates that the challenge is open or the current user is
    an admin.
    
    Fixes #176.

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -66,19 +66,19 @@ def render_index():
 
 
 @app.route('/attachment/<filename>')
-@utils.login_required
 def download(filename):
     """Download an attachment."""
 
     attachment = models.Attachment.query.get_or_404(filename)
-    valid = models.User.current().admin
+    cuser = models.User.current()
+    valid = cuser and cuser.admin
     for ch in attachment.challenges:
         if ch.unlocked:
             valid = True
             break
     if not valid:
         flask.abort(404)
-    app.logger.info('Download of %s by %r.', attachment, models.User.current())
+    app.logger.info('Download of %s by %r.', attachment, cuser or "Anonymous")
 
     return attachments.backend.send(attachment)
 
